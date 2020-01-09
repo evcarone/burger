593f830afbd40f4f5d35000bee50828bcd2656f3
E <evcarone@gmail.com>
2020-01-09 15:41:40 -0800

    updated with cats template - replaced with burger specific

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('cali', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('spicy', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('mine', FALSE);
