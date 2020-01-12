75f77b4677b19bfc61dcda42853554e07ca0022a
E <evcarone@gmail.com>
2020-01-11 22:03:28 -0800

    app working ... fixed controller.js file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,36 +2,40 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
+// Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burger.all(function(data) {
+
+    burger.all(function(data) {
     var hbsObject = {
-      burger: data
+      burgers: data
     };
+    console.log("*******")
     console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
 
 router.post("/api/burgers", function(req, res) {
+    console.log(req.body)
   burger.create([
     "burger_name"
   ], [
     req.body.name
   ], function(result) {
+      console.log(res)
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/api/burgers", function(req, res) {
+router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
-
+console.log(req.body.devoured)
   burger.update({
     devoured: true
   }, condition, function(result) {
